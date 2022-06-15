9f532d2d9c28287b5dcf7dcc70ec1cf88b48f837
Jacob-Yearoo <jacob.yearoo@hotmail.com>
2022-06-15 18:27:50 +0000

    add planned funtions to the scripts file to keep track of what needs to be done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,64 @@
-console.log("Working")
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome Challenger, we are ready for you.')
+})
+
+
+let questions = [
+    {
+        question: 'What is 2 + 2',
+        answer1: '2',
+        answer2: '4',
+        answer3: '7',
+        answer4: '10',
+        answer: 2,
+
+    },
+    {
+        question: 'tallest building in the world',
+        answer1: 'Burj Khalif',
+        answer2: '4',
+        answer3: '7',
+        answer4: '10',
+        answer: 1,
+
+    },
+    {
+        question: 'chocolate milk from cows',
+        answer1: '2',
+        answer2: '4',
+        answer3: '7%',
+        answer4: '10',
+        answer: 3,
+
+    },
+    {
+        question: 'power outages squirells',
+        answer1: '10-20%',
+        answer2: '4',
+        answer3: '7',
+        answer4: '10',
+        answer: 1,
+
+    }
+]
+let score = 0
+
+function nextQuestion() 
+
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function restartGame() {
+
+}
