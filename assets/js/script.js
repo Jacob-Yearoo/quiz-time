b3a89d8d89275c87b26e2a15457472d30e5405f2
Jacob-Yearoo <jacob.yearoo@hotmail.com>
2022-06-18 21:21:13 +0000

    write functions to cycle through different questions and add functionality to the next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,89 @@ document.addEventListener('DOMContentLoaded', function() {
     alert('Welcome Challenger, we are ready for you.')
 })
 
+const nextBtn = document.getElementById('next-btn')
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById('answers')
 
-let questions = [
+
+nextBtn.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
+
+
+
+const qList = [
     {
-        question: 'What is 2 + 2',
-        answer1: '2',
-        answer2: '4',
-        answer3: '7',
-        answer4: '10',
-        answer: 2,
-
+        question: 'in which continent are Chile, Argentina and Brazil?', answers: [
+            {text: 'South America', correct: true},
+            {text: 'North America', correct: false},
+            {text: 'Europe', correct: false},
+            {text: 'Australasia', correct: false},
+            
+        ]
     },
     {
-        question: 'tallest building in the world',
-        answer1: 'Burj Khalif',
-        answer2: '4',
-        answer3: '7',
-        answer4: '10',
-        answer: 1,
-
+        question: 'The Mad Hatter and the Cheshire Cat are characters in which famous book?', answers: [
+            {text: 'Winnie-The-Pooh', correct: false},
+            {text: "Charlotte's Web", correct: false},
+            {text: 'Charlie and the Chocolate Factory', correct: false},
+            {text: 'Alice in Wonderlanc', correct: true},
+            
+        ]
     },
     {
-        question: 'chocolate milk from cows',
-        answer1: '2',
-        answer2: '4',
-        answer3: '7%',
-        answer4: '10',
-        answer: 3,
-
+        question: 'What measurement scale is used to determine wind speed?', answers: [
+            {text: 'Beaufort Scale', correct: true},
+            {text: 'Richter Scale', correct: false},
+            {text: 'Synoptic Scale', correct: false},
+            {text: 'Gusting Scale', correct: false},
+            
+        ]
     },
     {
-        question: 'power outages squirells',
-        answer1: '10-20%',
-        answer2: '4',
-        answer3: '7',
-        answer4: '10',
-        answer: 1,
-
-    }
+        question: 'What other country, besides the US, uses the US dollar as its official currency?', answers: [
+            {text: 'Ecuador', correct: true},
+            {text: 'Canada', correct: false},
+            {text: 'Mexico', correct: false},
+            {text: 'United Kingdom', correct: false},
+            
+        ]
+    },
 ]
-let score = 0
 
-function nextQuestion() 
+let shuffledQuestions = qList.sort()
+let currentQuestionIndex = 0
 
 
-function checkAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('button')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', checkAnswer)
+        answerElement.appendChild(button)
+    });
 }
+
+function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function checkAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+}}
 
 function incrementScore() {
 
@@ -62,3 +98,5 @@ function incrementWrongScore() {
 function restartGame() {
 
 }
+
+
